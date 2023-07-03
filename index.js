6fd4d6fff9256ebcc6ba47071b8efb7e9a4ce2d0
mitchel buyengo <mitchelongachi63@gmail.com>
2023-07-03 16:50:55 +0300

    completed work

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+// Code your solutions in this fil
+function writeCards(names,event){
+    const new_arr=[];
+    for(let i = 0 ; i < names.length ; i++){
+        let msg = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+        new_arr.push(msg);
+    }
+    return new_arr;
+    }
+
+function countDown(number) {
+    let count = number;
+    while (count >= 0) {
+      console.log(count);
+      count--;
+    }
+  }
+  
+  countDown(10);
